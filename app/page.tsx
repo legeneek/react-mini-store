1053076afb316e4f80e298202158744547d63aec
Aaron Xu <legeneek@163.com>
2024-12-09 22:21:55 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,65 @@
 "use client";
 
 import { createStore } from "@/lib/store";
+import { useState } from "react";
 
-const useStore = createStore({ count: 1 });
+const useStore: any = createStore({ count: 1, name: "x" });
 
 export default function Home() {
+  const [show, setShow] = useState(true);
   return (
-    <div>
-      <CompA />
-      <CompB />
-      <CompC />
+    <div className="flex flex-col gap-4 p-4">
+      <div className="flex gap-2 items-center">
+        {show && <CompA />}
+        <CompC />
+      </div>
+      <div className="flex gap-2 items-center">
+        <CompB />
+        <CompD />
+      </div>
+      <div>
+        <button
+          className="border p-2 rounded-md"
+          onClick={() => {
+            setShow(!show);
+          }}
+        >
+          toggle
+        </button>
+      </div>
     </div>
   );
 }
 
 function CompA() {
-  console.log("render A");
-  const { data: count } = useStore("count");
+  console.log("render count");
+  const count = useStore.get("count");
   return <div>{count}</div>;
 }
 
 function CompB() {
-  console.log("render B");
-  const { data: count } = useStore("count");
-  return <div>{count * 2}</div>;
+  console.log("render name");
+  const name = useStore.get("name");
+  return <div>{name}</div>;
 }
 
 function CompC() {
-  console.log("render C");
-  const { data: count, setData: setCount } = useStore("count");
-  return <button onClick={() => setCount(count + 1)}>add</button>;
+  console.log("render button");
+  const setCount = useStore.set("count");
+  return (
+    <button
+      className="border p-2 rounded-md"
+      onClick={() => setCount((pre: any) => pre + 1)}
+    >
+      add
+    </button>
+  );
+}
+
+function CompD() {
+  console.log("render input");
+  const setName = useStore.set("name");
+  return (
+    <input className="border" onInput={(e: any) => setName(e.target.value)} />
+  );
 }
--- lib/store.ts
+++ lib/store.ts
@@ -4,37 +4,48 @@ export function createStore(target: any) {
   const depMap: any = new Map();
 
   function setter(key: any, val: any) {
-    target[key] = val;
-    const dep = depMap[key];
-    if (dep) {
-      const cbs = dep.values();
-      for (const f of cbs) {
-        f(val);
+    const pre = target[key]
+    if (pre === val) {
+      // no need to set
+    } else {
+      target[key] = val;
+      const dep = depMap[key];
+      if (dep) {
+        const cbs = dep.values();
+        for (const f of cbs) {
+          f(val);
+        }
       }
     }
   }
 
-  // useStore
-  return (key: any) => {
-    const [data, setData] = useState<any>(target[key]);
+  return {
+    get: (key: any) => {
+      const [data, setData] = useState<any>(target[key]);
+      useEffect(() => {
+        return () => {
+          depMap[key].delete(setData);
+        };
+      }, []);
 
-    if (!depMap[key]) {
-      depMap[key] = new Set();
-    }
-
-    if (!depMap[key].has(setData)) {
-      depMap[key].add(setData);
-    }
-
-    useEffect(() => {
-      return () => {
-        depMap[key].delete(setData);
-      };
-    }, []);
-
-    return {
-      data,
-      setData: (val: any) => setter(key, val),
-    };
+      if (!depMap[key]) {
+        depMap[key] = new Set();
+      }
+  
+      if (!depMap[key].has(setData)) {
+        depMap[key].add(setData);
+      }
+      return data
+    },
+    set: (key:any) => {
+      return (val: any) => {
+        if (typeof val == 'function') {
+          const v = val(target[key])
+          setter(key, v)
+        } else {
+          setter(key, val)
+        }
+      }
+    },
   };
 }
